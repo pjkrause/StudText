1f4770547d027d89f9da2bbeef66eee14193b608
Paul Krause <p.krause@surrey.ac.uk>
2014-11-26 10:19:46 +0000

    trying to sort out access to S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => 'AKIAJDMM3D7LWX5KMGVQ',
     :aws_secret_access_key  => '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',
-    :region                 => 'eu-west-1'
+    :region                 => 'eu-west-1',
+    :host                   => 'http://sarahstudholmetextiles.herokuapp.com',
+    :endpoint               => 's3-eu-west-1.amazonaws.com'
   }
   config.fog_directory  = 'sarahstudholmetextiles-assets'
 end
