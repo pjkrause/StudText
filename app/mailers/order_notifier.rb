b07ca729d40e17c34286ed8e028f22841a342517
Paul Krause <p.krause@surrey.ac.uk>
2014-12-03 22:16:31 +0000

    sorting the cc

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
-  # default from: "p.krause@surrey.ac.uk"
+  default from: "p.krause@surrey.ac.uk"
   default subject: "New Order Notification"
 
   # Subject can be set in your I18n file at config/locales/en.yml
